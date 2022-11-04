749b23c85e5a66f4e21010b90bb214ab0d3c0a98
Xavier Pinsonneault <xavier.pinsonneault@polymtl.ca>
2022-11-04 18:02:27 -0400

    probleme 2

--- chaine_de_caractere/decode/main.s
+++ chaine_de_caractere/decode/main.s
@@ -1,8 +1,8 @@
 .data
 string1:
-.ascii  "AazZWgFh tuvJKLYZHj" # Le message à decoder AazZWgFF donne une lettre ASCII, tuvJKLYZH donne l'autre. 
-string2:
-.ascii 
+.asciz  "AazZWgFh tuvJKLYZHj" # Le message à decoder AazZWgFF donne une lettre ASCII, tuvJKLYZH donne l'autre. 
+resultat:
+.asciz ""
 
 .text
 .globl main
@@ -13,6 +13,50 @@ mov %esp,%ebp
 push %ebx
 
 #Votre code ici
+CLD
+movl $0, %eax
+mov $string1, %esi
+lodsb
+
+calcul:
+
+#centaines
+movl $100, %ebx
+movl $0, %edx
+divl %ebx
+addb %al, resultat
+
+#dizaines et unitees
+movl %edx, %eax
+movl $10, %ebx
+movl $0, %edx
+divl %ebx
+addb %al, resultat
+addb %dl, resultat
+lodsb
+
+#verification si espace ou fin de string
+cmp $0, %eax
+je fin
+
+cmp $0x20, %eax
+jne calcul
+
+#print du premier charactere
+push $resultat
+call printf
+addl $4, %esp
+movb $0, resultat
+lodsb
+jmp calcul
+
+
+
+fin:
+#print du deuxieme charactere
+push $resultat
+call printf
+addl $4, %esp
 
 pop %ebx
 pop %ebp
