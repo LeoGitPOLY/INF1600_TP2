2e2395f54cb4a2db5a6b219ce4a4cf3a9d9bf145
Xavier Pinsonneault <xavier.pinsonneault@polymtl.ca>
2022-11-04 21:44:07 -0400

    approx flottante, il manque a savoir comment return le resultat

--- pi_approx/pi_approx_flottante/pi_approx_flottante.s
+++ pi_approx/pi_approx_flottante/pi_approx_flottante.s
@@ -11,7 +11,7 @@ signe1:
 .float 1
 signe2:
 .float -1
-pi:             # Valeur du ratio de pi initial
+pie:             # Valeur du ratio de pie initial
 .float 0
 cte: 
 .float 1
@@ -27,6 +27,64 @@ mov %esp,%ebp
 push %ebx
 
 #votre code
+movl iteration, %ecx
+movl $2, %ebx
+movl $0, %eax
+calcul:
+
+flds y
+flds i
+faddp
+
+flds x
+fmulp
+
+flds y
+fsubrp
+
+verificationpaire:
+pushl %eax
+movl $0, %edx
+divl %ebx
+cmp $0, %edx
+je addition
+jne soustraction
+
+boucle:
+popl %eax
+addl $1, %eax
+loop calcul
+jmp fin
+
+addition:
+flds signe1
+fdivp
+flds pie
+faddp
+fstps pie
+
+flds i
+flds cte
+faddp
+fstps i
+
+jmp boucle
+
+soustraction:
+flds signe2
+fdivp
+flds pie
+faddp
+fstps pie
+
+flds i
+flds cte
+faddp
+fstps i
+
+jmp boucle
+
+fin:
 
 pop %ebx
 pop %ebp
